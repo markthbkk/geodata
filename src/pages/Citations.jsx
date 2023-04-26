0138dc0823ee6a26d24fc0dc40764593853ba957
[Mark Thomas] <[mark.i.thomas@outlook.com]>
2023-04-26 21:14:39 +0700

    Commit 19

--- src/pages/Citations.jsx
+++ src/pages/Citations.jsx
@@ -21,23 +21,24 @@ useEffect(() => {
 
   return (
     <>
-      {GovData.map((item) => (
-        <Flex
-          direction="column"
-          key={item.displayName}
-          mb="1rem"
-          w="90%"
-          align="center"
-          boxShadow="md"
-        >
-          <Box bg="blue.900" color="white" p=".5rem">
-            {item.displayName}
-          </Box>
-          <Box border="1px solid black" p=".5rem">
-            {item.citation}
-          </Box>
-        </Flex>
-      ))}
+      <Flex justify="center" w="100%">
+        {GovData.map((item) => (
+          <Flex
+            direction="column"
+            key={item.displayName}
+            mb="1rem"
+            w="90%"
+            boxShadow="md"
+          >
+            <Box bg="blue.900" color="white" p=".5rem" w="100%">
+              {item.displayName}
+            </Box>
+            <Box border="1px solid black" p=".5rem" w="100%">
+              {item.citation}
+            </Box>
+          </Flex>
+        ))}
+      </Flex>
     </>
   );
     
