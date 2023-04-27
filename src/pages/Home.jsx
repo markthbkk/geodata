5d714ce3a110a7d146404e59959de81f693840d2
[Mark Thomas] <[mark.i.thomas@outlook.com]>
2023-04-27 19:51:39 +0700

    Add Home Logo 4

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,64 +1,19 @@
 import { Flex, Image } from "@chakra-ui/react";
 import QOGLogo from "/QOG-Logo.png"
-// import { useLoaderData } from "react-router-dom";
-// import Countries from "../components/Countries";
-// import Search from "../components/Search";
-// import { useState } from "react";
+
 
 export default function Home() {
-  // const [showSearch, setShowSearch] = useState(true);
-  // const [showCountries, setShowCountries] = useState(false);
-  //   const [countriesFilter, setcountriesFilter] = useState({});
-  //   const [filterType, setFilterType] = useState("")
-
-  // const handleClickHideSearch = () => {
-  //   setShowSearch(false);
-  // };
-  //   const handleClickShowCountries = (filteredCountriesObject) => {
-  //       setShowCountries(true);
-  //       if (filteredCountriesObject.SubRegion?.subregion) {
-
-  //           setcountriesFilter(filteredCountriesObject.SubRegion.subregion)
-
-  //           setFilterType("subregion");
-  //       }
-
-  //       console.log(filteredCountriesObject.PartialName)
-
-  //       if (filteredCountriesObject.PartialName) {
-  //       setcountriesFilter(filteredCountriesObject.PartialName)
-
-  //       setFilterType("searchstring")
-  //   }
-  // };
-
-  //   const hideCountries = () => {
-
-  //       setShowCountries(false);
-  //   }
-
-  // const allCountries = useLoaderData();
+  
 
   return (
-    <Flex w="100%" alignItems="center" h="75vh">
-      {/* {showSearch && (
-            <Search
-              prepareCountriesFilter={handleClickShowCountries}
-              hideCountries={hideCountries}
-            />
-          )}
-          {showCountries && (
-            <Countries
-              allCountries={allCountries}
-              countriesFilter={countriesFilter}
-              filterType={filterType}
-            />
-          )} */}
+    <Flex w="100%" alignItems="center" justifyContent="center" h="75vh">
+      
       <Image
         boxSize="50%"
         objectFit="cover"
         src={QOGLogo}
         alt="Quality Of Governement Project Logo"
+        border="2px solid black"
       />
     </Flex>
   );
