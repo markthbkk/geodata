b500561592f8691693ca4c20ce03643a86d04086
[Mark Thomas] <[mark.i.thomas@outlook.com]>
2025-05-27 21:46:21 +0700

    refactor for Coolify 3

--- vite.config.js
+++ vite.config.js
@@ -3,8 +3,6 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  esbuild: {
-    drop: ["console", "debugger"],
-  },
   plugins: [react()],
+  base: "/",
 });
